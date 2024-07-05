1e91b17fbcb7cd37e81ddd32362b2d4a51513489
MOWNEASH R S <125719771+Mowneash@users.noreply.github.com>
2024-07-05 17:14:17 +0530

    Update 07.v

--- Assignments/Assignment-5/07.v
+++ Assignments/Assignment-5/07.v
@@ -1,10 +1,10 @@
-//7. Write Verilog code for generating an infinite clock signal with a 50% Duty cycle having a time period of 10ns using a forever loop.`timescale 1us/1ps
+//7. Write Verilog code for generating an infinite clock signal with a 50% Duty cycle having a time period of 10ns using a forever loop.
+`timescale 1ns/1ps
 module timescale();
 reg clk;
 initial begin 
 clk=0;
-forever #0.005 clk=~clk;
-#1$finish;
+forever #5 clk=~clk;
 end
 endmodule
   
